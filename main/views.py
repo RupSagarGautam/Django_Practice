06082288d54bce4e839008603198ca67476d7276
RupSagarGautam <rupsagargautam@gmail.com>
2025-07-24 11:45:38 +0545

    Updated Admin and Writer Dashboard

--- main/views.py
+++ main/views.py
@@ -1,59 +1,102 @@
+from collections import defaultdict
+import json
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from blogs.models import Blog, BlogStats
-from collections import defaultdict
+from users.models import User
+
 
 def landingPage(request):
-    return render(request,'pages/index.html')
+    return render(request, "pages/index.html")
+
 
 def aboutPage(request):
-    return render(request, 'pages/aboutus/about_page.html')
+    return render(request, "pages/aboutus/about_page.html")
+
 
 def loginPage(request):
-    return render (request, 'pages/auth/login.html')
+    return render(request, "pages/auth/login.html")
+
 
 def signupPage(request):
-    return render(request, 'pages/auth/signup.html')
+    return render(request, "pages/auth/signup.html")
+
 
 def blogPage(request):
-    blogs  =  Blog.objects.filter(status='Active').order_by('-created_at')
-    return render(request, 'pages/blogs/blogs.html', {'blogs':blogs})
+    blogs = Blog.objects.filter(status="Active").order_by("-created_at")
+    return render(request, "pages/blogs/blogs.html", {"blogs": blogs})
 
-@login_required(login_url='/auth/log-in/')
+
+@login_required(login_url="/auth/log-in/")
 def profilePage(request):
-    return render(request, 'pages/auth/profile.html')
+    return render(request, "pages/auth/profile.html")
 
-@login_required(login_url='/auth/log-in/')
+
+@login_required(login_url="/auth/log-in/")
 def dashboard(request):
-    user_blogs = Blog.objects.filter(author=request.user)
-    blogstats = BlogStats.objects.filter(blog__in=user_blogs).order_by('created_at')
+    if request.user.profile.role == "Admin":
+        return redirect("/admin/dashboard")
 
+    user_blogs = Blog.objects.filter(author=request.user)
+    blogstats = BlogStats.objects.filter(blog__in=user_blogs).order_by("created_at")
     click_data = defaultdict(int)
     unique_data = defaultdict(int)
-
     for b in blogstats:
-        date_str = b.created_at.strftime('%Y-%m-%d')
+        date_str = b.created_at.strftime("%Y-%m-%d")
         click_data[date_str] += b.blog_clicks
         unique_data[date_str] += b.unique_views
 
-    click_date = list(click_data.keys())  # Assumes both click_data and unique_data have same dates
-    blogclicks = [click_data[date] for date in click_date]
-    unique_views = [unique_data[date] for date in click_date]
+    # blog stats
+    blog_clicks = list(click_data.values())
+    click_date = list(click_data.keys())
+    unique_views = list(unique_data.values())
 
-    return render(request, 'pages/dashboard/writer/dashboard.html', {
+    context = {
+        "blog_clicks": blog_clicks,
         "click_date": click_date,
-        "blogclicks": blogclicks,
-        "unique_views": unique_views
-    })
-    
+        "unique_views": unique_views,
+    }
+    return render(request, "pages/dashboard/writer/dashboard.html", context)
+
+
 def blogList(request):
-    blogs = Blog.objects.filter( author = request.user).order_by('-created_at')
-    return render(request, 'pages/dashboard/writer/blogList.html', {'blogs':blogs})
+    blogs = Blog.objects.filter(author=request.user).order_by("-created_at")
+    return render(request, "pages/dashboard/writer/blogList.html", {"blogs": blogs})
+
 
 @login_required(login_url="/auth/log-in")
 def adminDashboard(request):
-    if request.user.profile.role=="Admin":
-        return render(request, 'pages/dashboard/admin/dashboard.html')
+    if request.user.profile.role == "Admin":
+        blogstats = BlogStats.objects.all().order_by("created_at")
+        click_data = defaultdict(int)
+        unique_data = defaultdict(int)
+        
+        for b in blogstats:
+            date_str = b.created_at.strftime("%Y-%m-%d")
+            click_data[date_str] += b.blog_clicks
+            unique_data[date_str] += b.unique_views
+        
+        # blog stats
+        blog_clicks = list(click_data.values())
+        click_date = list(click_data.keys())
+        unique_views = list(unique_data.values())
+        
+        # counting users
+        users = User.objects.all()
+        user_data = defaultdict(int)
+        for user in users:
+            date_str = user.date_joined.strftime("%Y-%m-%d")
+            user_data[date_str] += 1
+            # user stats
+        user_date = list(user_data.keys())
+        user_count = list(user_data.values())
+        context = {
+            "user_date": user_date,
+            "user_count": user_count,
+            "blog_clicks": blog_clicks,
+            "click_date": click_date,
+            "unique_views": unique_views,
+        }
+        return render(request, "pages/dashboard/admin/dashboard.html", context)
     else:
-        return redirect('/writer/dashboard')
-
+        return redirect("/writer/dashboard")
